e6180baac2e9ff48f510287024540f91a856aafc
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2022-12-09 04:44:26 -0800

    My C_Projects commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -writing  a line of code that prints with printf instead of puts
+ *
+ * Return =0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -My codes to print out bytes
+ * Return: 0
+*/
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof());
+        printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof());
+        printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof());
+        printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof());
+        printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof());	
+
+	                                          	
+
+	return (0);
+}
