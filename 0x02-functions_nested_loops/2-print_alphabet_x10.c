df431c3c9aa4e4a2eb8b1946695b96aa72687bf3
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2022-12-14 20:26:11 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char letter;
 
-	while (count++ <=)
+	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-		_putcher(letter);
-	 _putcher('\n');
+		_putchar(letter);
+	 _putchar('\n');
 	}
 }
