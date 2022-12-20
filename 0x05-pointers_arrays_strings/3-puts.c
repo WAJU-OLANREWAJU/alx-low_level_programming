cdcccea133a580ca261cd23c08d48d7d2a444630
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2022-12-19 19:45:03 -0800

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: the string
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	int a = 0
+
+	while (str[a] != '\0')
+	{
+		_putchar(stra[a]);
+		a++
+	}
+}
