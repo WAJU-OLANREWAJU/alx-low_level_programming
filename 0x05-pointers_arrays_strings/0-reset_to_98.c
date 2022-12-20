811b07ea52beeda188582b77f89199ec9527ecfd
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2022-12-19 18:30:57 -0800

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/*
+ * reset_to_98 - This project updates the parameter of an int using pointer.
+ * @n= int to update
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
+
