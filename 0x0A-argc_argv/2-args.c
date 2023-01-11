5dfe4df8afb0921df340cdedcbe5fb4c203a6af3
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-01-11 05:52:47 -0800

    My project on coomand line argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints all arguments it receives.
+ * @argc: argument count
+ * @argv: argument
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	printf("%s\n", argv[1]);
+	 printf("%s\n", argv[2]);
+	  printf("%s\n", argv[3]);
+	   printf("%s\n", argv[4]);
+	    printf("%s\n", argv[5]);
+	     printf("%s\n", argv[6]);
+	      printf("%s\n", argv[7]);
+
+
+
+	return (0);
+}
