aa876655769834f27938956fb5f1fc19ce51a8d7
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-01-13 02:30:36 -0800

    My proje t task on CLA

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 
 /**
- * main - this program multiplies two numbers.
+ * main - multiplies two numbers.
  * @argc: argument count
- * @argv: argument
+ * @argv: arguments
  *
  * Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-
 	int x, y;
 
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -22,9 +20,7 @@ int main(int argc, char *argv[])
 
 	x = atoi(argv[1]);
 	y = atoi(argv[2]);
-
 	printf("%d\n", x * y);
 
 	return (0);
 }
-
