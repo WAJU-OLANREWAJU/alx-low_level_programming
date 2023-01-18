b2b5edba201b6addf3485abdd387f985b69f29a6
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-01-18 14:53:08 -0800

    My Alx SWE task on Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main - this program defines a struct.
+ *
+ *@
+ *
+ *Return: Always 0
+ */
+
+struct dog{
+char *name;
+float age;
+char *owner;
+};
+
+int main (void){
+
+return (0);
+}
