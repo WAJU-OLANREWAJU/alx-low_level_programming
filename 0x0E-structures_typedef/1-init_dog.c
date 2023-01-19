7695d195835096b840ca39577b6ca000825cb7fc
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-01-18 21:44:06 -0800

    My task on struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.com>
+#include <main.h>
+
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner){
+
+struct dog my_dog;
+
+my_dog.name[];
+my_dog.age;
+my_dog.owner[];
+
+}
