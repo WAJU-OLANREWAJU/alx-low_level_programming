0d5cd3505c054ab07ec218375b7a7a1800fe2c3c
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-02-01 02:25:20 -0800

    My alx project solution to Singly linked list tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,13 @@ size_t print_list(const list_t *h)
 	count = 0;
 	while (h != NULL)
 	{
+
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
 		count++;
 
