9319437c0a6a505e547b382aae1b9cfa3738142d
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-02-03 06:54:47 -0800

    More singly linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,11 +9,12 @@ include "lists.h"
 
 size_t listint_len(const listint_t *h)
 {
+	const listint_t *element = h;
 	size_t count = 0;
 
-	while (h != NULL)
+	while (element != NULL)
 	{
-		h = h->next;
+		element = element->next;
 		count++;
 	}
 	return (count);
