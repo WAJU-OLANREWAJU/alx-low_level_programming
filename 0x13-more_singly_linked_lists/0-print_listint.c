6563efcc99fe2bbc08ccbf2cae048197b6d684ba
WAJU-OLANREWAJU <wajuolanrewaju@gmail.com>
2023-02-03 04:44:42 -0800

    My project on more singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
 
-/*
- * print_listint - prints all the elements of a linked list.
+/**
+ * print_listint - prints all the elements of a linked list
  * @h: head of the list
  *
- * Return: the number of nodes.
+ * Return: the number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *elements = h;
@@ -14,10 +13,9 @@ size_t print_listint(const listint_t *h)
 
 	while (elements != NULL)
 	{
-		printf("%d\n", element->n);
+		printf("%d\n", elements->n);
 		count += 1;
 		elements = elements->next;
-	
 	}
 	return (count);
 }
